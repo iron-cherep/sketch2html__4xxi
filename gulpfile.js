9bde82eacf2127b56822411f2ac97685738c48df
iron-cherep <jacobxblue@gmail.com>
2017-02-16 22:04:29 +0300

    добавил оптимизацию svg

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ var rename = require("gulp-rename");
 var del = require("del");
 var sourcemaps = require("gulp-sourcemaps");
 var imagemin = require("gulp-imagemin");
+var svgmin = require("gulp-svgmin");
 
 var postcss = require("gulp-postcss");
 var cssnext = require("postcss-cssnext");
@@ -73,6 +74,15 @@ gulp.task("images", function() {
 });
 
 ////
+//Оптимизирует SVG-изображения. Не заменяет оригинальных файлов.
+////
+gulp.task("svgmin", function() {
+  return gulp.src("img/**/*.svg")
+    .pipe(svgmin())
+    .pipe(gulp.dest("build/img"));
+});
+
+////
 //Активирует browser-sync с заданными параметрами.
 ////
 gulp.task("serve", function() {
@@ -105,9 +115,8 @@ gulp.watch("build/js/*.js").on("change", server.reload); //следит за js-
 gulp.task("copy", function() {
   return gulp.src([
     "fonts/**/*.{woff,woff2}",
-    "img/**",
+    "img/**/*.{png,jpg,gif}",
     "js/**",
-    "icons/*", "*.ico", "*.png", "*.xml",
     "*.html"
   ], {
     base: "."
@@ -129,6 +138,7 @@ gulp.task("build", function (fn) {
   run(
     "clean",
     "copy",
+    "svgmin",
     "style",
     fn
   );
