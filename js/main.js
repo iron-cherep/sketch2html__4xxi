fa50f9d510635c76ad44174b82526acabd22c0cd
iron-cherep <jacobxblue@gmail.com>
2017-02-20 15:07:41 +0300

    закончил базовые планшетные стили

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
 window.onload = function() {
 
+  // сворачивает списки при загрузке страницы
+  // (по умолчанию развёрнуты для доступности без js)
   $(".list__info").next().slideToggle();
 
+
+  // переключает состояние списка по клику на заголовок
   $(".list__info").click(function() {
     $(this).next().slideToggle();
+  });
+
+  // скрывает всплывающее меню с мобильным приложением
+  $("#close-app-popup").click(function() {
+    $(this).parent().slideToggle();
   });
 
 }
