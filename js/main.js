cc2c52a6064a30c7c4c8c4190a4257ab475d3514
iron-cherep <jacobxblue@gmail.com>
2017-02-21 01:55:30 +0300

    реализовал всплывающее окно с социальными кнопками

--- js/main.js
+++ js/main.js
@@ -4,8 +4,6 @@ window.onload = function() {
   // (по умолчанию развёрнуты для доступности без js)
   $(".list__info").next().slideToggle();
 
-
-
   // переключает состояние списка по клику на заголовок
   $(".list__info").click(function() {
     $(this).next().slideToggle();
@@ -14,6 +12,19 @@ window.onload = function() {
   // скрывает всплывающее меню с мобильным приложением
   $("#close-app-popup").click(function() {
     $(this).parent().slideToggle();
+  });
+
+  // закрывает окно с кнопками соцсетей
+  $("#social-popup__close").click(function() {
+    $("#social-popup__window").addClass("popup--hidden");
+  });
+
+  // открывает окно с кнопками соцсетей
+  $(".note__button--share").click(function() {
+    $("#social-popup__window").removeClass("popup--hidden");
+  });
+  $(".social__toggle").click(function() {
+    $("#social-popup__window").removeClass("popup--hidden");
   });
 
 }
